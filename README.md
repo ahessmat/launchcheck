d1fa9e1b8c9432a34da915c92f1945c2ac1642ac
ahessmat <ahessmat@gmail.com>
2021-07-28 13:33:29 -0400

    Updated test.py -Added haversine method for calculating distance (in km) between 2 sets of lat/long -Changed basemap from bluemarble() to shadedrelief(); looks better when zoomed in -Updated test.py to actively query Celestrak for most recent TLE data and plot satellites; currently using scatterplot

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # launchcheck
+
+## TODO:
+-Implement a method for ingesting TLE data
+-Implement a means for specifying/plotting launch destination
+-Restrict plots of debris to only those that pose risk to launch site/time
+-Create server that independently queries for TLE data and serves to test.py; this will speed up performance
--- test.py
+++ test.py
@@ -10,9 +10,27 @@ import os.path
 from os import path
 import argparse	#needed for parsing commandline arguments
 from datetime import datetime	#Needed for checking launch day
+import requests	#needed to query CelesTrak
+from math import radians,cos,sin,asin,sqrt
+from tqdm import tqdm	#Added for loading bar
+
+#Function for calculating the distance between 2 points given their latitudes/longtitudes
+def haversine(lat1,lon1,lat2,lon2):
+	R = 6372.8	#Earth radius km
+	dLat = radians(lat2 - lat1)
+	dLon = radians(lon2 - lon1)
+	lat1 = radians(lat1)
+	lat2 = radians(lat2)
+
+	a = sin(dLat/2)**2 + cos(lat1)*cos(lat2)*sin(dLon/2)**2
+	c = 2*asin(sqrt(a))
+
+	return R*c
 
 #References
 #https://space.stackexchange.com/questions/27688/how-do-i-determine-the-ground-track-period-of-a-leo-satellite
+#https://www.nasa.gov/mission_pages/station/news/orbital_debris.html
+	#4x50x50km box
 
 today = datetime.now()
 #Get default date
@@ -24,15 +42,8 @@ mi = today.minute
 def_date = str(yy) + "-" + str(mm) + "-" + str(dd)
 def_time = str(hh)+str(mi)
 
-print("DEFDATE: " + def_date)
-print("DEFTIME: " + def_time)
-
+#Configure command line arguments
 argp = argparse.ArgumentParser(description="Program for observing space debris relative to a launch position")
-#argp.add_argument("-y", "--year", type=int, default=today.year)
-#argp.add_argument("-m", "--month", type=int, default=today.month)
-#argp.add_argument("-d", "--day", type=int, default=today.day)
-#argp.add_argument("--hour", type=int, default=today.hour)
-#argp.add_argument("-m", "--minute", type=int, default=today.minute)
 argp.add_argument("-d", "--date", help="Date of launch (YYYY-MM-DD)", default=def_date)
 argp.add_argument("-t", "--time", help="Time of launch (0000 thru 2359)", default=def_time)
 parsed=argp.parse_args()
@@ -69,6 +80,7 @@ else:
 	elif month % 2 == 1 and (day > 31 or day < 1):
 		print("[-] Day doesn't exist")
 		sys.exit(0)
+
 #Verify that the time is formatted appropriately
 time = parsed.time
 #Time provided should be formatted as HHMM
@@ -89,8 +101,7 @@ else:
 		print("[-] Minute incorrectly formatted")
 		sys.exit(0)
 
-print(hour)
-print(minute)
+
 
 #Initiate some variables to be used in the construction of Earth's representation
 halfpi, pi, twopi = [f*np.pi for f in (0.5, 1, 2)]
@@ -99,19 +110,51 @@ degs, rads = 180/pi, pi/180
 #Loader() can specify where to load our .bsp file to
 #Defaults to wherever the program is ran
 #load = Loader('~/Desktop/launchcheck/SkyData')
-
 data = load('de421.bsp')	#skyfield intuits that it needs to download this file; see "Ephemeris download links"
 ts = load.timescale()
 
 earth = data['earth']
 
+#TODO
+#Implement function to call Celestrak and scrape TLE data
+
+fig = plt.figure(figsize=(10,8))
+#m = Basemap(projection='cyl', llcrnrlat=-90, urcrnrlat=90, llcrnrlon=-180, urcrnrlon=180,)
+m = Basemap(projection='cyl', llcrnrlat=19, urcrnrlat=31, llcrnrlon=-92, urcrnrlon=-72,)
+#m.bluemarble()
+m.shadedrelief()
+
+active_sats = requests.get("http://celestrak.com/NORAD/elements/active.txt")
+
+tle1 = active_sats.text.splitlines()
+for idx,line in enumerate(tqdm(tle1)):
+	if idx%3 == 0:
+		name = line.strip()	#Removes trailing white space
+		L1 = tle1[idx+1]
+		L2 = tle1[idx+2]
+
+		#minutes = np.arange(60. * 24 * 1)         # (1) day
+		#time = ts.utc(yr, month, day, hour, range(minute-5,minute+5))  # start June 1, 2018
+		time = ts.utc(yr, month, day, hour, minute)
+
+		ISS = EarthSatellite(L1,L2)
+		sub = ISS.at(time).subpoint()
+		lon = sub.longitude.degrees
+		lat = sub.latitude.degrees
+		#breaks   = np.where(np.abs(lon[1:]-lon[:-1]) > 30)  #don't plot wrap-around
+		#lon, lat    = lon[:-1], lat[:-1]
+		#lon[breaks] = np.nan
+
+		#plt.plot(lon,lat)
+		plt.scatter(lon,lat)
+
+###################
+"""
 #Test TLE data of ISS
-TLE = """1 25544U 98067A   18157.92534723  .00001336  00000-0  27412-4 0  9990
-2 25544  51.6425  69.8674 0003675 158.7495 276.7873 15.54142131116921"""
+#TLE goes here
 
 L1, L2 = TLE.splitlines()
-#TODO
-#Implement commandline options for specifying the date of launch
+
 #Give options for year, month, day
 #minutes = np.arange(60. * 24 * 7)         # (7) days
 minutes = np.arange(60. * 24 * 1)         # (1) day
@@ -132,7 +175,7 @@ m = Basemap(projection='cyl', llcrnrlat=-90, urcrnrlat=90, llcrnrlon=-180, urcrn
 m.bluemarble()
 
 plt.plot(lon,lat)
-
+"""
 #x, y = m(-122.3, 47.6)
 #plt.plot(x, y, 'ok', markersize=5)
 #plt.text(x, y, ' Seattle', fontsize=12);
