666107e8486d3269e4cf8b429f4fd019cd749252
ahessmat <ahessmat@gmail.com>
2021-07-29 01:30:01 -0400

    Performed numerous updates to the processing of debris; added commandline options for specifying desired launch orbit; need to still optimize TLE ingestion and calculate nearest misses

--- test.py
+++ test.py
@@ -40,13 +40,37 @@ dd = today.day
 hh = today.hour
 mi = today.minute
 def_date = str(yy) + "-" + str(mm) + "-" + str(dd)
+
+#datetime uses 24hr clock, but 01-09 get abbreviated as 1-9
+if hh < 10:
+	hh = "0"+str(hh)
+if mi < 10:
+	mi = "0"+str(mi)
 def_time = str(hh)+str(mi)
+print("CURRENT DATE: " + def_date)
+print("CURRENT TIME: " + def_time)
 
 #Configure command line arguments
 argp = argparse.ArgumentParser(description="Program for observing space debris relative to a launch position")
 argp.add_argument("-d", "--date", help="Date of launch (YYYY-MM-DD)", default=def_date)
 argp.add_argument("-t", "--time", help="Time of launch (0000 thru 2359)", default=def_time)
+argp.add_argument("-s", "--site", choices=['1','2'], help="Select launch site: (1) Vandenberg, (2) Cape Canaveral", default="1")
+group = argp.add_mutually_exclusive_group(required=True)
+#group.add_argument("-a", "--alt", help="The max altitude (in km) the launch will achieve")
+group.add_argument("-L", help="LEO: Launch will stay under 2000km", action='store_true')
+group.add_argument("-M", help="MEO: Launch altitude is 2000km < alt < 35,786km", action='store_true')
+group.add_argument("-G", help="GEO: Launch altitude is greater than 36,786km; this is the default", action='store_true')
 parsed=argp.parse_args()
+
+LEO = False
+MEO = False
+GEO = False
+if parsed.L:
+	LEO = True
+if parsed.M:
+	MEO = True
+if parsed.G:
+	GEO = True
 
 #Verify that the date is properly formatted
 date = parsed.date
@@ -101,6 +125,18 @@ else:
 		print("[-] Minute incorrectly formatted")
 		sys.exit(0)
 
+#Select the launch site
+#TODO: add more sites
+#Dev note: each degree of latitude is ~111km at equator; ~85km +- 40deg 
+site = parsed.site
+#LATITUDE, LONGITUTE, SITENAME
+sites = {
+	"1" : (34.7373367,-120.5843126, "Vandenberg"),	#Vandenberg
+	"2" : (28.396837,-80.605659, "Cape Canaveral")	#Cape Canaveral
+}
+slat,slong,siteName = sites[site]
+#slat = int(slat)
+#slong = int(slong)
 
 
 #Initiate some variables to be used in the construction of Earth's representation
@@ -108,25 +144,32 @@ halfpi, pi, twopi = [f*np.pi for f in (0.5, 1, 2)]
 degs, rads = 180/pi, pi/180
 
 #Loader() can specify where to load our .bsp file to
+#This pulls planet-relative data for timescale
 #Defaults to wherever the program is ran
-#load = Loader('~/Desktop/launchcheck/SkyData')
 data = load('de421.bsp')	#skyfield intuits that it needs to download this file; see "Ephemeris download links"
 ts = load.timescale()
-
 earth = data['earth']
 
 #TODO
 #Implement function to call Celestrak and scrape TLE data
-
 fig = plt.figure(figsize=(10,8))
 #m = Basemap(projection='cyl', llcrnrlat=-90, urcrnrlat=90, llcrnrlon=-180, urcrnrlon=180,)
-m = Basemap(projection='cyl', llcrnrlat=19, urcrnrlat=31, llcrnrlon=-92, urcrnrlon=-72,)
+#m = Basemap(projection='cyl', llcrnrlat=19, urcrnrlat=31, llcrnrlon=-92, urcrnrlon=-72,)
+m = Basemap(projection='cyl', llcrnrlat=(slat-10), urcrnrlat=(slat+10), llcrnrlon=(slong-10), urcrnrlon=(slong+10),)
 #m.bluemarble()
 m.shadedrelief()
+plt.scatter(slong,slat)
+plt.annotate(siteName, (slong,slat))
 
-active_sats = requests.get("http://celestrak.com/NORAD/elements/active.txt")
+#active_sats = requests.get("http://celestrak.com/NORAD/elements/iridium-33-debris.txt")
+#active_sats = requests.get("http://celestrak.com/NORAD/elements/active.txt")
 
-tle1 = active_sats.text.splitlines()
+#tle1 = active_sats.text.splitlines()
+
+tle1 = """ISS
+1 25544U 98067A   18157.92534723  .00001336  00000-0  27412-4 0  9990
+2 25544  51.6425  69.8674 0003675 158.7495 276.7873 15.54142131116921""".splitlines()
+
 for idx,line in enumerate(tqdm(tle1)):
 	if idx%3 == 0:
 		name = line.strip()	#Removes trailing white space
@@ -134,49 +177,51 @@ for idx,line in enumerate(tqdm(tle1)):
 		L2 = tle1[idx+2]
 
 		#minutes = np.arange(60. * 24 * 1)         # (1) day
-		#time = ts.utc(yr, month, day, hour, range(minute-5,minute+5))  # start June 1, 2018
-		time = ts.utc(yr, month, day, hour, minute)
+		time = ts.utc(yr, month, day, hour, range(minute-5,minute+5))  # start June 1, 2018
+		#time = ts.utc(yr, month, day, hour, minute)
 
-		ISS = EarthSatellite(L1,L2)
-		sub = ISS.at(time).subpoint()
+		satl = EarthSatellite(L1,L2)
+		satlloc = satl.at(time)
+		satl_alt = satlloc.distance().km - 6371	#Get satellite altitude by subtracing earth's mean radius (km)
+		if LEO and satl_alt.all() > 2000:
+			continue
+		if MEO and satl_alt.all() > 36786:
+			continue
+		sub = satlloc.subpoint()
 		lon = sub.longitude.degrees
 		lat = sub.latitude.degrees
-		#breaks   = np.where(np.abs(lon[1:]-lon[:-1]) > 30)  #don't plot wrap-around
-		#lon, lat    = lon[:-1], lat[:-1]
-		#lon[breaks] = np.nan
+		#print((lon))
+		breaks   = np.where(np.abs(lon[1:]-lon[:-1]) > 30)  #don't plot wrap-around
+		lon, lat    = lon[:-1], lat[:-1]
+		lon[breaks] = np.nan
 
-		#plt.plot(lon,lat)
-		plt.scatter(lon,lat)
+		#t = haversine(lat,lon,slat,slong)
+		#print(t)
+		plt.plot(lon,lat, label=name)
+
+		#"""
+		#Check the first longtitude
+		if np.isnan(lon[0]) or lon[0] < m.llcrnrlon or lon[0] > m.urcrnrlon:
+			end = lon[len(lon)-1]
+			#Check the last longtitude
+			if np.isnan(end) or end < m.llcrnrlon or end > m.urcrnrlon:
+				#If both fall outside of our boundary, don't plot it
+				continue
+		#Do the same with latitudes
+		if np.isnan(lat[0]) or lat[0] < m.llcrnrlat or lat[0] > m.urcrnrlat:
+			end = lat[len(lat)-1]
+			if np.isnan(end) or end < m.llcrnrlat or end > m.urcrnrlat:
+				continue
+		#"""
+		
+		#plt.scatter(lon,lat)
+	else:
+		continue
 
 ###################
-"""
-#Test TLE data of ISS
-#TLE goes here
 
-L1, L2 = TLE.splitlines()
-
-#Give options for year, month, day
-#minutes = np.arange(60. * 24 * 7)         # (7) days
-minutes = np.arange(60. * 24 * 1)         # (1) day
-#time = ts.utc(yr, month, day, 0, minutes)  # start June 1, 2018
-time = ts.utc(yr, month, day, hour, range(minute-5,minute+5))  # start June 1, 2018
-#time = ts.now()
-
-ISS = EarthSatellite(L1,L2)
-sub = ISS.at(time).subpoint()
-lon = sub.longitude.degrees
-lat = sub.latitude.degrees
-breaks   = np.where(np.abs(lon[1:]-lon[:-1]) > 30)  #don't plot wrap-around
-lon, lat    = lon[:-1], lat[:-1]
-lon[breaks] = np.nan
-
-fig = plt.figure(figsize=(10,8))
-m = Basemap(projection='cyl', llcrnrlat=-90, urcrnrlat=90, llcrnrlon=-180, urcrnrlon=180,)
-m.bluemarble()
-
-plt.plot(lon,lat)
-"""
 #x, y = m(-122.3, 47.6)
 #plt.plot(x, y, 'ok', markersize=5)
 #plt.text(x, y, ' Seattle', fontsize=12);
+#plt.legend()
 plt.show()
